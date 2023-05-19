6152ddc2bcb4da6de30d8d825c28868f643ae052
Rafael Lemos <rhplemos@gmail.com>
2023-05-19 14:48:09 -0300

    feat: base project

---
+++ app/src/main/java/com/example/user_repository_infos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.user_repository_infos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
