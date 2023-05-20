a965630010a54760ec1a36484fc85ce95678058a
Rafael Lemos <rhplemos@gmail.com>
2023-05-20 10:16:26 -0300

    feat: main first form

--- app/src/main/java/com/example/user_repository_infos/MainActivity.kt
+++ app/src/main/java/com/example/user_repository_infos/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.user_repository_infos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.user_repository_infos.databinding.ActivityMainBinding
+import com.example.user_repository_infos.scenes.userInfo.UserInfoActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() = binding.run {
+        seeUserInfoBTN.setOnClickListener {
+            openUserInfoActivity()
+        }
+    }
+
+    private fun openUserInfoActivity() {
+        val intent = Intent(this, UserInfoActivity::class.java)
+        startActivity(intent)
     }
 }
